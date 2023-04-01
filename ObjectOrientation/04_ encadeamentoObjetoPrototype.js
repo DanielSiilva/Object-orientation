b5eb19eb56e69f58430c3cbd043d62f5c47cd43f
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-01 16:58:56 -0300

    Feat: encadeamento do objeto prototype

---
+++ ObjectOrientation/04_ encadeamentoObjetoPrototype.js
@@ -0,0 +1,5 @@
+// Existe um 'Classe' Global dentro de cada função chamada Objetc que tem o seu proprio prototype 
+
+// Object: tem acesso a todos os metodos do objeto disponiveis na linguagem
+
+// Tudo herda do prototype object por padrão => mas podemos mudar 
