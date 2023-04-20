b08bc3fa89e88e9de3e4f445b15c3e38aa0ee26b
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-19 21:41:16 -0300

    Feat: adicionando class user

--- ObjectOrientation/entidades/Usuario.js
+++ ObjectOrientation/entidades/Usuario.js
@@ -1,3 +1,54 @@
-export class Usuario {
-    
-}
+export class User {
+    #id;
+    #name;
+    #email;
+    #photo;
+  
+    constructor({ id, name, email, photo } = {}) {
+      this.#id = id;
+      this.#name = name;
+      this.#email = email;
+      this.#photo = photo;
+    }
+  
+    get id() {
+      return this.#id;
+    }
+  
+    set id(id) {
+      this.#id = id;
+    }
+  
+    get name() {
+      return this.#name;
+    }
+  
+    set name(name) {
+      this.#name = name;
+    }
+  
+    get email() {
+      return this.#email;
+    }
+  
+    set email(email) {
+      this.#email = email;
+    }
+  
+    get photo() {
+      return this.#photo;
+    }
+  
+    set photo(photo) {
+      this.#photo = photo;
+    }
+  
+    toObject() {
+      return {
+        id: this.id,
+        name: this.name,
+        email: this.email,
+        photo: this.photo
+      }
+    }
+  }
